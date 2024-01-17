544da1afdc4ae65abdd625ec75be485dfbf5ac01
imshubhamchavan <imshubhamchavan@gmail.com>
2024-01-17 15:39:34 +0530

    adding favicon for project and creating new component for cart items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,13 @@
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.style.scss"
 
 const CartDropdown = () => {
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {[].map(item => <CartItem cartItem={item}/>)}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,23 @@
 import { createContext, useState } from 'react';
 
+const addCartItems = (cartItems, productToAdd) => {
+
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsOpen: () => {},
+  setIsOpen: () => { },
+  cartItems: [],
+  addItemToCart: () => { }
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItems(cartItems, productToAdd))
+  }
   const value = { isCartOpen, setIsCartOpen };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };

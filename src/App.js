210c606dce8cedefb207b53660c5f486847ac5f5
imshubhamchavan <imshubhamchavan@gmail.com>
2024-01-26 22:05:55 +0530

    add some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
-import Footer from './routes/footer/footer.component';
+
 
 const App = () => {
   return (
